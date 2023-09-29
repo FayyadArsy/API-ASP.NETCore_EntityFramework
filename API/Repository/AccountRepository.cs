7313514c20963492ce9b294d51fe776e70ae1d52
Fayyad Arsy Nandi <fayyad.nandi@berca.co.id>
2023-09-29 13:57:13 +0700

    add repository for each models

---
+++ API/Repository/AccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using API.Repository.Interface;
+
+namespace API.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public int Delete(string NIK)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account Get(string NIK)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/Repository/EducationRepository.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using API.Repository.Interface;
+
+namespace API.Repository
+{
+    public class EducationRepository : IEducationRepository
+    {
+        public int Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Education> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Education Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Education education)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Education education)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
